42d1ada007d6ceb8ef039a3c8d9347ab1df5822c
Kertu <kertu.koss@gmail.com>
2019-05-14 23:48:16 +0800

    chaging the dot game dimesnions

--- src/p5sketches/sketch4.js
+++ src/p5sketches/sketch4.js
@@ -7,22 +7,24 @@ p.setup = () => {
     p.background(126);
   }
   p.draw = ()=> {
-    let rand = p.random(1,50)
+    let rand = p.random(1,75)
     p.ellipse(p.mouseX-150, p.mouseY, rand, rand);   
-    p.ellipse(p.mouseX/2, p.mouseY*2, rand, rand); 
     p.ellipse(p.mouseX*3, p.mouseY, rand, rand); 
     p.ellipse(p.mouseX, p.mouseY*4, rand, rand); 
-    rand = p.random(1,50)
+    rand = p.random(1,100)
     p.ellipse(p.mouseX*2, p.mouseY/2, rand, rand);
     p.ellipse(p.mouseX/5, p.mouseY*2, rand, rand); 
-    p.ellipse(p.mouseX*2, p.mouseY+150, rand, rand);
-    rand = p.random(1,50)
+    
+    rand = p.random(1,100)
     p.ellipse(p.mouseX/2, p.mouseY*4, rand, rand); 
     p.ellipse(p.mouseX*2, p.mouseY/2, rand, rand);
-    rand = p.random(1,50)
-    p.ellipse(p.mouseX*4, p.mouseY*4, rand, rand);
+    p.ellipse(p.mouseX*0.3, p.mouseY*0.4, rand, rand);
+    rand = p.random(1,100)
+    p.ellipse(p.mouseX*2, p.mouseY*2, rand, rand);
     p.ellipse(p.mouseX/2, p.mouseY*4, rand, rand); 
     p.ellipse(p.mouseX*2.5, p.mouseY*4, rand, rand);
+    p.ellipse(p.mouseX*0.3, p.mouseY*4, rand, rand);
+
   }
   p.mousePressed = () => {
     p.ellipse(p.mouseX,p.mouseY,50,50)
